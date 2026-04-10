d5350b8452a3bb77d4a5406567d836e2434723da
Jarrod Wandrey <jwandrey@mybrycer.com>
2026-04-10 11:48:28 -0500

    Adds watch method and youtube link validation in clipboard.py

--- YoutubeLinkDownload/clipboard.py
+++ YoutubeLinkDownload/clipboard.py
@@ -1,1 +1,18 @@
 # Logic for detecting clipboard contents and validation
+import pyperclip
+import time
+
+def start_watching():
+    previous_clipboard = ""
+    while True:
+        current_clipboard = pyperclip.paste()
+        if current_clipboard != previous_clipboard and is_youtube_link(current_clipboard):
+            # Process the new link
+            previous_clipboard = current_clipboard
+
+        time.sleep(1)  # Check clipboard every second
+
+# Looking online there is a crazy regex way to validate youtube links
+# Might look into that later, for now just check if the string contains "youtube.com"
+def is_youtube_link(link:str) -> bool:
+    return "youtube.com" in link.lower()
--- YoutubeLinkDownload/main.py
+++ YoutubeLinkDownload/main.py
@@ -1,12 +1,16 @@
-"""
-Module description goes here.
-"""
-
+import argparse
+from . import clipboard, history, download, gui
 
 def main():
-    """Main entry point of the program."""
-    print("Hello, World!")
+    # Creates an arugment to allow for the program to run headless for multiple downloads, or with a GUI for single downloads
+    parser = argparse.ArgumentParser(description="YouTube Link Downloader")
+    parser.add_argument('--watch', action='store_true', help='Watch clipboard for links')
+    args = parser.parse_args()
 
+    if args.watch:
+        clipboard.watch_clipboard()
+    else:
+        pass
 
 if __name__ == "__main__":
     main()
